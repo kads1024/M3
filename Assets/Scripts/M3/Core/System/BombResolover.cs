1f437d4243fd9cdefa68d17a4e856bd49e8104ad
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-12-16 20:34:02 +0800

    Added Initial Bomb Resolver

---
+++ Assets/Scripts/M3/Core/System/BombResolover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using M3.Core.Domain;
+
+namespace M3.Core.System
+{
+    public sealed class BombResolver : IBombResolver
+    {
+        private static readonly (int dx, int dy)[] Offsets =
+        {
+            // center
+            ( 0,  0),
+
+            // vertical (±1, ±2)
+            ( 0,  1), ( 0,  2),
+            ( 0, -1), ( 0, -2),
+
+            // horizontal (±1, ±2)
+            ( 1,  0), ( 2,  0),
+            (-1,  0), (-2,  0),
+
+            // diagonals (±1)
+            ( 1,  1), (-1,  1),
+            ( 1, -1), (-1, -1)
+        };
+
+        public IReadOnlyCollection<Position> Resolve(
+            BoardState board,
+            Position bombPosition)
+        {
+            var results = new HashSet<Position>();
+
+            foreach (var (dx, dy) in Offsets)
+            {
+                int x = bombPosition.X + dx;
+                int y = bombPosition.Y + dy;
+
+                if (board.IsInside(x, y))
+                {
+                    results.Add(new Position(x, y));
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Assets/Scripts/M3/Core/System/IBombResolver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using M3.Core.Domain;
+
+namespace M3.Core.System
+{
+    public interface IBombResolver
+    {
+        IReadOnlyCollection<Position> Resolve(
+            BoardState board,
+            Position bombPosition);
+    }
+
+}
