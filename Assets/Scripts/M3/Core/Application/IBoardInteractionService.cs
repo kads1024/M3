09536eeb3f54417b4a9ecf131372f70bf2c8d28e
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-12-16 19:35:55 +0800

    Create Initial Board Interaction Service

---
+++ Assets/Scripts/M3/Core/Application/IBoardInteractionService.cs
@@ -0,0 +1,12 @@
+﻿using M3.Core.Domain;
+
+namespace M3.Core.Application
+{
+    public interface IBoardInteractionService
+    {
+        SwapResult TrySwap(
+            BoardState board,
+            Position a,
+            Position b);
+    }
+}
