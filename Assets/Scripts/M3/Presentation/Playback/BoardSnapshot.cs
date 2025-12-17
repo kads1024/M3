3c6c345dfe131c69f2c63092674b6f509270d440
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-12-17 23:42:45 +0800

    Add Board Snapshots so that we can animate in between snapshots

---
+++ Assets/Scripts/M3/Presentation/Playback/BoardSnapshot.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using M3.Core.Domain;
+
+namespace M3.Presentation.Playback
+{
+    public sealed class BoardSnapshot
+    {
+        public readonly Dictionary<Position, GemState> Cells;
+
+        public BoardSnapshot(BoardState board)
+        {
+            Cells = new Dictionary<Position, GemState>();
+
+            for (int x = 0; x < board.Width; x++)
+            for (int y = 0; y < board.Height; y++)
+            {
+                var cell = board.GetCell(x, y);
+                if (!cell.IsEmpty)
+                {
+                    Cells[new Position(x, y)] = cell.Gem.Clone();
+                }
+            }
+        }
+    }
+}
