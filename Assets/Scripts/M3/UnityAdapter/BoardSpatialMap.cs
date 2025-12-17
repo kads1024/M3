28cad4ac8f1de3300684f4da967e88bf6cf38a9d
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-12-17 21:11:52 +0800

    Update spatial map so that cell calculations are based on center of cell and not bottom left

--- Assets/Scripts/M3/UnityAdapter/BoardSpatialMap.cs
+++ Assets/Scripts/M3/UnityAdapter/BoardSpatialMap.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using M3.Core.Domain;
+
 namespace M3.UnityAdapter
 {
     public sealed class BoardSpatialMap
     {
+        private readonly BoardState _board;
         private readonly float _cellSize;
         private readonly Vector3 _origin;
-        private readonly BoardState _board;
-        
+
+        private readonly float _halfCell;
+
         public float CellSize => _cellSize;
         
         public BoardSpatialMap(BoardState board, float cellSize, Vector3 origin)
@@ -15,24 +18,23 @@ namespace M3.UnityAdapter
             _board = board;
             _cellSize = cellSize;
             _origin = origin;
-            
-            Debug.Log($"BoardSpatialMap created at origin: ({_origin.x},  {_origin.y}, {_origin.z})");
+            _halfCell = cellSize * 0.5f;
         }
-
+        
         public Vector3 GridToWorld(Position pos)
         {
             return _origin + new Vector3(
-                pos.X * _cellSize,
-                pos.Y * _cellSize,
+                (pos.X * _cellSize) + _halfCell,
+                (pos.Y * _cellSize) + _halfCell,
                 0f);
         }
-
+        
         public bool TryWorldToGrid(Vector3 world, out Position position)
         {
             Vector3 local = world - _origin;
 
-            int x = Mathf.FloorToInt(local.x / _cellSize);
-            int y = Mathf.FloorToInt(local.y / _cellSize);
+            int x = Mathf.FloorToInt((local.x) / _cellSize);
+            int y = Mathf.FloorToInt((local.y) / _cellSize);
 
             if (_board.IsInside(x, y))
             {
