f86b802b8dc7460eca0678e3362c46022a6a8c07
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2025-12-17 21:47:29 +0800

    Added GemViews

--- Assets/Scripts/M3/Presentation/Board/BoardView.cs
+++ Assets/Scripts/M3/Presentation/Board/BoardView.cs
@@ -1,38 +1,31 @@
 using UnityEngine;
 using VContainer;
+using System.Collections.Generic;
 using M3.Core.Domain;
 using M3.Application.Bootstrap;
 using M3.UnityAdapter;
+using M3.Presentation.Gem;
 
 namespace M3.Presentation.Board
 {
     public sealed class BoardView : MonoBehaviour
     {
         [Inject] private BoardBootstrapper _bootstrapper;
-        private BoardSpatialMap _spatialMap;
 
         private BoardState _board;
+        private BoardSpatialMap _spatialMap;
+
+        private readonly Dictionary<Position, GemView> _gemViews = new();
 
         private void Start()
         {
             _board = _bootstrapper.Board;
             _spatialMap = _bootstrapper.SpatialMap;
-            
-            if (_board == null)
-            {
-                Debug.LogError("BoardView: BoardState is null");
-                return;
-            }
-            
-            if (_spatialMap == null)
-            {
-                Debug.LogError("BoardView: BoardSpatialMap is null");
-                return;
-            }
-            RenderBoard();
+
+            RenderInitialBoard();
         }
 
-        private void RenderBoard()
+        private void RenderInitialBoard()
         {
             for (int x = 0; x < _board.Width; x++)
             for (int y = 0; y < _board.Height; y++)
@@ -41,34 +34,24 @@ namespace M3.Presentation.Board
                 if (cell.IsEmpty)
                     continue;
 
-                CreateGemView(cell.Gem, new Position(x, y));
+                var pos = new Position(x, y);
+                CreateGemView(pos, cell.Gem);
             }
         }
 
-        private void CreateGemView(GemState gem, Position pos)
+        private void CreateGemView(Position pos, GemState gem)
         {
             var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            go.name = $"Gem ({pos.X},{pos.Y})";
-
+            go.name = $"GemView ({pos.X},{pos.Y})";
             go.transform.SetParent(transform);
+
+            var view = go.AddComponent<GemView>();
+            view.Initialize(pos, gem);
+
             go.transform.position = _spatialMap.GridToWorld(pos);
             go.transform.localScale *= _spatialMap.CellSize;
-            
-            var renderer = go.GetComponent<Renderer>();
-            renderer.material.color = ColorFor(gem.Color);
-        }
-
-        private static Color ColorFor(GemColor color)
-        {
-            return color switch
-            {
-                GemColor.Red => Color.red,
-                GemColor.Blue => Color.blue,
-                GemColor.Green => Color.green,
-                GemColor.Yellow => Color.yellow,
-                GemColor.Purple => Color.magenta,
-                _ => Color.white
-            };
+                
+            _gemViews[pos] = view;
         }
     }
 }
